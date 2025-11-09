0d8e9c8a833d7bba312e2ce83cd5a73901b732b3
Du Zhe <duzhe0211@gmail.com>
2025-11-09 22:02:11 +0800

    add kernel/09-samba

---
+++ kernel/09-samba.sh
@@ -0,0 +1,6 @@
+./scripts/config --enable NETWORK_FILESYSTEMS
+./scripts/config --enable CIFS
+./scripts/config --enable NETWORK_FILESYSTEMS
+./scripts/config --enable CIFS_ALLOW_INSECURE_LEGACY
+./scripts/config --enable CIFS_XATTR
+./scripts/config --enable CIFS_POSIX
